129db4890b56545471c151760f6465cd3874abf3
Thais Regina <devpro.thais@gmail.com>
2023-04-23 20:08:58 -0300

    Insert BeanValidatio and receive th data request

--- src/main/java/br/com/projetoThais/mvc/controller/PedidoController.java
+++ src/main/java/br/com/projetoThais/mvc/controller/PedidoController.java
@@ -1,6 +1,12 @@
 package br.com.projetoThais.mvc.controller;
 
+import br.com.projetoThais.mvc.dto.RequisicaoNovoPedido;
+import br.com.projetoThais.mvc.model.Pedido;
+import br.com.projetoThais.mvc.repository.PedidoRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +15,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("pedido")
 public class PedidoController {
 
+    @Autowired
+    private PedidoRepository repository;
+
     @GetMapping("/formulario")
     public String formulario() {
         return "pedido/formulario";
     }
 
     @PostMapping("novo")
-    public String novo() {
+    public String novo(@Valid RequisicaoNovoPedido requisicao, BindingResult result) {
+        if (result.hasErrors()) {
+            return "pedido/formulario";
+        }
+        Pedido novoPedido = requisicao.toPedido();
+        repository.save(novoPedido);
+
         return "pedido/formulario";
     }
 
